9af125562344b65f2cc2a21c506a54051a958c83
Jungwon  Kim <kimwonny8@gmail.com>
2023-01-07 18:41:10 +0900

    todo test

---
+++ backend/src/main/java/com/example/backend/controller/TodoForm.java
@@ -0,0 +1,19 @@
+package com.example.backend.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoForm {
+
+    @NotEmpty(message = "내용은 필수입니다.")
+    private String item;
+
+    private String date;
+
+    private boolean completed;
+
+    private String time;
+}
