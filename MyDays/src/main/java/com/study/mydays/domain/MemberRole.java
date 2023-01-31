6279c0271d7d644db049160f5282e8d61c897c35
Jungwon  Kim <kimwonny8@gmail.com>
2023-01-31 15:17:46 +0900

    delete

--- MyDays/src/main/java/com/study/mydays/domain/MemberRole.java
+++
@@ -1,5 +0,0 @@
-package com.study.mydays.domain;
-
-public enum MemberRole {
-    USER, ADMIN
-}
--- MyDays/src/main/java/com/study/mydays/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.mydays.repository;
-
-import com.study.mydays.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
